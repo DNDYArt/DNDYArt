0fa241b2dbdeed9c390432977f384f4b07f16a7e
Craig Leslie <cleslie25@gmail.com>
2022-05-11 09:52:42 -0500

    saving before commit

--- client/src/components/SignInPane.js
+++ client/src/components/SignInPane.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import '../assets/modal.css'
 
 function SignInPane(props) {
   
@@ -13,26 +13,28 @@ function SignInPane(props) {
       <>
         <div className='component--pane-blur' onClick={props.toggle}></div>
   
-        <form onSubmit={handleSubmit} className='component--form'>
-          <h3>Sign In</h3>
-          <div className='form-group'>
-            <label for='username'>Username</label>
-            <input type='text' name='username' placeholder='Username' />
-          </div>
-          <div className='form-group'>
-            <label for='email'>Email</label>
-            <input type='email' name='email' placeholder='Email'/>
-          </div>
-          <div className='form-group'>
-            <label for='Password'>Password</label>
-            <input type='password' name='Password' placeholder='Password'/>
-          </div>
-          <div className='form-group'>
-            <label for='confpass'>Confirm Password</label>
-            <input type='password' name='confpass' placeholder='Confirm Password'/>
-          </div>
-          <button type='submit'>Signup</button>
-        </form>
+        <div className="component">
+            <form onSubmit={handleSubmit} className='component--form'>
+            <h3>Sign In</h3>
+            <div className='form-group'>
+                <label for='username'>Username</label>
+                <input type='text' name='username' placeholder='Username' />
+            </div>
+            <div className='form-group'>
+                <label for='email'>Email</label>
+                <input type='email' name='email' placeholder='Email'/>
+            </div>
+            <div className='form-group'>
+                <label for='Password'>Password</label>
+                <input type='password' name='Password' placeholder='Password'/>
+            </div>
+            <div className='form-group'>
+                <label for='confpass'>Confirm Password</label>
+                <input type='password' name='confpass' placeholder='Confirm Password'/>
+            </div>
+            <button type='submit'>Signup</button>
+            </form>
+        </div>
       </>
     )
   }
