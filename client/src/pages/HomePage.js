357c1cfecf84c5ec7af6d56d76450e0a4c74ea80
Devin Wulf <devinlwulf@gmail.com>
2022-05-11 02:29:09 -0500

    add Parralax code

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Container } from "react";
 import { Box, Image, Button, Heading } from "@chakra-ui/react";
 import Img from '../components/greek-bust.png'
 
@@ -24,6 +24,31 @@ const HomePage = (props) => {
 						<Button className="enter" colorScheme='black' variant='outline' size='lg'>explore</Button>
 					</Box>
 				</section>
+        {/* <!-- Second-Fourth --> */}
+        <section>
+        <Container scrollAxis="vertical">
+      <div className={styles.images}>
+        <Parallax speed={4} className={styles.parallaxImage}>
+          <Image src={imageFile1} />
+        </Parallax>
+        <Parallax speed={-4} className={styles.parallaxImage}>
+          <Image src={imageFile2} />
+        </Parallax>
+        <Parallax speed={4} className={styles.parallaxImage2}>
+          <Image src={imageFile4} />
+        </Parallax>
+        <Parallax speed={-3} className={styles.parallaxImage}>
+          <Image src={imageFile5} />
+        </Parallax>
+        <Parallax speed={3} className={styles.parallaxImage}>
+          <Image src={imageFile3} />
+        </Parallax>
+        <Parallax speed={6} className={styles.parallaxImage2}>
+          <Image src={imageFile6} />
+        </Parallax>
+      </div>
+    </Container>
+        </section>
 			</main>
 		</>
 	);
