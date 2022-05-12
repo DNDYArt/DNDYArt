c5e8cc5e4e6dd81c663ce1b60a994a42eaa55187
Devin Wulf <devinlwulf@gmail.com>
2022-05-12 15:21:50 -0500

    complete main shop design, add slider next

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,44 +1,103 @@
-import React from "react";
-import { Grid, GridItem } from "@chakra-ui/react";
+import { React, useState } from "react";
+import {
+	Grid,
+	GridItem,
+	Button,
+	FormControl,
+	FormLabel,
+	FormHelperText,
+	FormErrorMessage,
+	NumberInput,
+	NumberInputField,
+	NumberInputStepper,
+	NumberIncrementStepper,
+	NumberDecrementStepper,
+} from "@chakra-ui/react";
 import Feature from "../components/images/abstract-3.jpg";
 
 function Shop() {
+	const [input, setInput] = useState("");
+
+	const handleInputChange = (e) => setInput(e.target.value);
+  
+  const isError = input === "";
+  
+  function Submit() {
+    if(NumberInput > 12000) {
+      handleInputChange();
+    } else {
+      return isError;
+    }
+  }
+
 	return (
 		<>
 			<main className="shop-main">
-				{/* <!-- flex, row, col-12 --> */}
 				<Grid className="shop-section" templateColumns="repeat(12)">
-					{/* <!-- image / timer, col-5 --> */}
 					<GridItem w="600px" className="shop-container" colSpan={5}>
-						{/* <!-- col-5 --> */}
 						<img
 							src={Feature}
 							className="feature-img"
 							alt="abtract painting"
 						/>
+						{/* add Countdown function, restart @ 48hrs, */}
 						<h3 className="timer">Time Left: </h3>
 					</GridItem>
-					<GridItem w="600px" className="shop-container" colSpan={5}>
-						{/* <!-- col-5, align: center --> */}
+					<GridItem
+						w="600px"
+						className="shop-description"
+						colSpan={5}
+					>
 						<GridItem className="feature-header" colSpan={5}>
-							<h2 className="feature-artist">artist name</h2>
+							{/* Add Artist Name from ArtistDB */}
+							<h2 className="feature-artist">Steve Johnson</h2>
 							<GridItem className="feature-name">
+								{/* Add Feature Name from FeatureDB */}
 								{/* <!-- flex, row, align: baseline --> */}
-								<h3 className="feature-title">title</h3>
-								<p className="feature-year">year</p>
+								<h3 className="feature-title">
+									the long hallway
+								</h3>
+								<p className="feature-year">(2014)</p>
 							</GridItem>
-							<h4 className="feature-medium">medium</h4>
+							<h4 className="feature-medium">Oil on canvas.</h4>
 						</GridItem>
 						<GridItem className="feature-about">
-							<p className="para">About</p>
+							<p className="para">About the feature from the artist:</p>
+							<br></br>
+							<p className="para2">
+								Life, uh, finds a way. This thing comes<br></br>
+								fully loaded: AM/FM radio, reclining bucket
+								seats,<br></br>
+								and… power windows.
+							</p>
 						</GridItem>
 						<GridItem className="bid-info">
-							<h4>The starting bid is:</h4>
-							<h3>The curent bid is:</h3>
+							<h4 className="bid-start">The starting bid is:</h4>
+							<h3 className="bid-current">The current bid is:</h3>
 						</GridItem>
-						<GridItem className="bid-btn">
+						<GridItem className="bid-form">
 							{/* <!-- add margin to match section height --> */}
-							<button>Place Bid</button>
+							<FormControl isInvalid={isError}>
+								<FormLabel htmlFor="amount">Amount</FormLabel>
+								<NumberInput min={0}>
+									<NumberInputField id="amount" />
+									<NumberInputStepper>
+										<NumberIncrementStepper />
+										<NumberDecrementStepper />
+									</NumberInputStepper>
+								</NumberInput>
+								{!isError ? (
+									<FormHelperText>
+										Enter the amount you wish to bid on this
+										feature.
+									</FormHelperText>
+								) : (
+									<FormErrorMessage>
+										A bid amount is required.
+									</FormErrorMessage>
+								)}
+							</FormControl>
+							<Button onClick={Submit} className="bid-btn">Place Bid</Button>
 						</GridItem>
 					</GridItem>
 				</Grid>
