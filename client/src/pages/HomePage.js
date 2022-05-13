11664d8ac3ac95e6ad12f19d1f0a0667a46dc6a6
Vidalatan <vidalatan@gmail.com>
2022-05-12 21:29:11 -0500

    Fixed Footer bug

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -14,6 +14,7 @@ import {
 	SlideFade,
 	useDisclosure,
 } from "@chakra-ui/react";
+import Footer from '../components/Footer/Footer'
 
 // Import Pictures
 import Bust from "../components/images/greek-bust.png";
